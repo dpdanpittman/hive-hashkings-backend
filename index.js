39035fb2b746144ec3322b941933bf0b7354e740
Daniel Pittman <dpdanpittman@gmail.com>
2020-11-17 23:26:10 -0800

    Update index.js

--- index.js
+++ index.js
@@ -356,11 +356,11 @@ app.get('/delegation/:user', (req, res, next) => {
 
 app.listen(port, () => console.log(`HASHKINGS token API listening on port ${port}!`))
 var state;
-var startingBlock = ENV.STARTINGBLOCK || 48787493; //GENESIS BLOCK
+var startingBlock = ENV.STARTINGBLOCK || 48788034; //GENESIS BLOCK
 const username = ENV.ACCOUNT || 'hashkings'; //account with all the SP
 const key = dhive.PrivateKey.from(ENV.skey); //active key for account
 const sh = ENV.sh || '';
-const ago = ENV.ago || 48787493;
+const ago = ENV.ago || 48788034;
 const prefix = ENV.PREFIX || 'qwoyn_'; // part of custom json visible on the blockchain during watering etc..
 var client = new dhive.Client([
     //"https://hive.roelandp.nl",
@@ -531,17 +531,6 @@ function startApp() {
                 ipfsSaveState(num, JSON.stringify(state))
             }
 
-            if (num % 28800 === 0) {
-                var d = parseInt(state.bal.c)
-                state.bal.r += state.bal.c
-                if (d) {
-                    state.refund.push(['xfer', 'qwoyn', d, 'Producer Cut'])
-                    state.bal.c = 0
-                    d = 0
-                        //state.refund.push(['xfer', 'qwoyn-chest', state.bal.c, 'Warchest'])
-                    state.refund.push(['power', username, state.bal.b, 'Power to the people!'])
-                }
-            }
         })
         // search for qwoyn_harvest from user on blockchain since genesis
     processor.on('harvest', function(json, from) {
