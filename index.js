6ba2d12cc004980052cd5a5b0a13b6964db6499a
Daniel Pittman <dpdanpittman@gmail.com>
2020-12-26 22:13:56 -0800

    added pathwise

--- index.js
+++ index.js
@@ -75,7 +75,10 @@ const ipfs = new IPFS({
 
 */
 const init = require('./state');
+var Pathwise = require('./pathwise');
+var level = require('level');
 
+var store = new Pathwise(level('./db', { createIfEmpty: true }));
 const app = express();
 const port = ENV.PORT || 3000;
 const wkey = ENV.wkey;
@@ -357,11 +360,11 @@ app.get('/delegation/:user', (req, res, next) => {
 
 app.listen(port, () => console.log(`HASHKINGS token API listening on port ${port}!`))
 var state;
-var startingBlock = ENV.STARTINGBLOCK || 49901093; //GENESIS BLOCK
+var startingBlock = ENV.STARTINGBLOCK || 49905580; //GENESIS BLOCK
 const username = ENV.ACCOUNT || 'hashkings'; //account with all the SP
 const key = dhive.PrivateKey.from(ENV.skey); //active key for account
 const sh = ENV.sh || '';
-const ago = ENV.ago || 49901093;
+const ago = ENV.ago || 49905580;
 const prefix = ENV.PREFIX || 'qwoyn_'; // part of custom json visible on the blockchain during watering etc..
 var client = new dhive.Client([
     "https://hive.roelandp.nl"
@@ -577,14 +580,12 @@ function startApp() {
             } catch {
                 console.log(" line 544 ") 
             }
-            /*try{
-                if (num % 50 === 0 && processor.isStreaming()) {
-                    ipfsSaveState(num, JSON.stringify(state))
-                    console.log("saved state at " + num)
-                }
-            } catch (e){
-                console.log(" line 552 ") 
-            }   */
+            if (num % 100 === 1) {
+                store.get([], function(err, data) {
+                    const blockState = Buffer.from(JSON.stringify([num, data]))
+                    ipfsSaveState(num, blockState)
+                })
+            }
         })
         // search for qwoyn_harvest from user on blockchain since genesis
     processor.on('harvest', function(json, from) {
