1cd9a3ac72499911bee5874656e8b202c0910ea8
Daniel Pittman <dpdanpittman@gmail.com>
2021-01-16 22:35:32 -0800

    Update index.js

--- index.js
+++ index.js
@@ -48,6 +48,7 @@ var hivejs = require('@hiveio/hive-js');
 var axios = require('axios');
 const config = require('./config');
 var steemState = require('./processor');
+var contract = require('./contract.js')
 var steemTransact = require('steem-transact');
 var fs = require('fs');
 const cors = require('cors');
@@ -169,10 +170,10 @@ app.get('/u/:user', (req, res, next) => {
 
 app.listen(port, () => console.log(`HASHKINGS API listening on port ${port}!`))
 var state;
-var startingBlock = ENV.STARTINGBLOCK || 50511450; //GENESIS BLOCK
+var startingBlock = ENV.STARTINGBLOCK || 50511655; //GENESIS BLOCK
 const username = ENV.ACCOUNT || 'hashkings'; //account with all the SP
 const key = dhive.PrivateKey.from(ENV.skey); //active key for account
-const ago = ENV.ago || 50511450;
+const ago = ENV.ago || 50511655;
 const prefix = ENV.PREFIX || 'qwoyn_'; // part of custom json visible on the blockchain during watering etc..
 var client = new dhive.Client([
     "https://hive.roelandp.nl"
