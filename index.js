6ad6df14497d572d2799067e8c05ea6b677d6de6
Daniel Pittman <dpdanpittman@gmail.com>
2021-01-16 23:45:43 -0800

    Update index.js

--- index.js
+++ index.js
@@ -170,10 +170,10 @@ app.get('/u/:user', (req, res, next) => {
 
 app.listen(port, () => console.log(`HASHKINGS API listening on port ${port}!`))
 var state;
-var startingBlock = ENV.STARTINGBLOCK || 50512878; //GENESIS BLOCK
+var startingBlock = ENV.STARTINGBLOCK || 50513058; //GENESIS BLOCK
 const username = ENV.ACCOUNT || 'hashkings'; //account with all the SP
 const key = dhive.PrivateKey.from(ENV.skey); //active key for account
-const ago = ENV.ago || 50512878;
+const ago = ENV.ago || 50513058;
 const prefix = ENV.PREFIX || 'qwoyn_'; // part of custom json visible on the blockchain during watering etc..
 var client = new dhive.Client([
     "https://hive.roelandp.nl"
@@ -1139,6 +1139,8 @@ function startApp() {
 
                                 // create one seed nft and return type of seed
                                 contract.createOneSeed(hivejs, 5, json.from).then(res => {
+                                    console.log("the variable res is below")
+                                    console.log(res)
                                     const { data } = res
                                     console.log("pulled data displaying below")
                                     console.log(data)
