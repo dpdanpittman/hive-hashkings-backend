e9d60775af2e0200b5418f68a252de2357df84e1
Daniel Pittman <dpdanpittman@gmail.com>
2021-01-21 23:02:45 -0800

    Update index.js

--- index.js
+++ index.js
@@ -170,10 +170,10 @@ app.get('/u/:user', (req, res, next) => {
 
 app.listen(port, () => console.log(`HASHKINGS API listening on port ${port}!`))
 var state;
-var startingBlock = ENV.STARTINGBLOCK || 50655655; //GENESIS BLOCK
+var startingBlock = ENV.STARTINGBLOCK || 50655822; //GENESIS BLOCK
 const username = ENV.ACCOUNT || 'hashkings'; //account with all the SP
 const key = dhive.PrivateKey.from(ENV.skey); //active key for account
-const ago = ENV.ago || 50655655;
+const ago = ENV.ago || 50655822;
 const prefix = ENV.PREFIX || 'qwoyn_'; // part of custom json visible on the blockchain during watering etc..
 var client = new dhive.Client([
     "https://hive.roelandp.nl"
@@ -1055,7 +1055,7 @@ function startApp() {
                                 state.bal.c += c
                                 state.cs[`${json.block_num}:${json.from}`] = `${json.from} purchased an asia bundle`
                              
-                                } else if (want === 'jamaica_bundle' && amount > (state.stats.prices.bundles.jamaicaBundle * 1000) - 3000 &&  amount < (state.stats.prices.bundle.jamaicaBundle * 1000) + 3000 && state.stats.supply.land.jamaica >= 1) {
+                                } else if (want === 'jamaica_bundle' && amount > (state.stats.prices.bundles.jamaicaBundle * 1000) - 3000 &&  amount < (state.stats.prices.bundles.jamaicaBundle * 1000) + 3000 && state.stats.supply.land.jamaica >= 1) {
                                 
                                 // update total number of plots
                                 state.users[json.from].plotCount++
