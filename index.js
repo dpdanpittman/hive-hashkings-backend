f179e9cb3c4a39c2ec100514f43424b064c26ac0
Daniel Pittman <dpdanpittman@gmail.com>
2021-02-10 19:42:42 -0800

    update

--- index.js
+++ index.js
@@ -170,10 +170,10 @@ app.get('/u/:user', (req, res, next) => {
 
 app.listen(port, () => console.log(`HASHKINGS API listening on port ${port}!`))
 var state;
-var startingBlock = ENV.STARTINGBLOCK || 51222150; //GENESIS BLOCK
+var startingBlock = ENV.STARTINGBLOCK || 51226665; //GENESIS BLOCK
 const username = ENV.ACCOUNT || 'hashkings'; //account with all the SP
 const key = dhive.PrivateKey.from(ENV.skey); //active key for account
-const ago = ENV.ago || 51222150;
+const ago = ENV.ago || 51226665;
 const prefix = ENV.PREFIX || 'qwoyn_'; // part of custom json visible on the blockchain during watering etc..
 var client = new dhive.Client([
     "https://hive.roelandp.nl"
