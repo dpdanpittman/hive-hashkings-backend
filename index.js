17735406cffcffed98fe0b93fe80efb97253c8b5
Daniel Pittman <dpdanpittman@gmail.com>
2021-03-07 23:52:16 -0800

    Update index.js

--- index.js
+++ index.js
@@ -206,10 +206,10 @@ app.get('/u/:user', (req, res, next) => {
 
 //app.listen(port, () => console.log(`HASHKINGS API listening on port ${port}!`))
 var state;
-var startingBlock = ENV.STARTINGBLOCK || 51949751; //GENESIS BLOCK
+var startingBlock = ENV.STARTINGBLOCK || 51950041; //GENESIS BLOCK
 const username = ENV.ACCOUNT || 'hashkings'; //account with all the SP
 const key = dhive.PrivateKey.from(ENV.skey); //active key for account
-const ago = ENV.ago || 51949751;
+const ago = ENV.ago || 51950041;
 const prefix = ENV.PREFIX || 'qwoyn_'; // part of custom json visible on the blockchain during watering etc..
 var client = new dhive.Client([
     "https://hive.roelandp.nl"
@@ -301,9 +301,9 @@ function userList() {
         let username = farmerArray[i]
         if (state.users[username]){
             contract.getReport(axios).then((res) => {
-                let report = res[4];
+                let report = res[4].username;
                 for (const property in report) {
-                data = report[username][property].seeds
+                data = report[property].seeds
                 console.log(data)
                     //state.users.seeds = data
             }
