db5c4449f649348ed43a957340d7567f04aa1f10
Daniel Pittman <dpdanpittman@gmail.com>
2021-03-08 00:26:34 -0800

    Update index.js

--- index.js
+++ index.js
@@ -206,10 +206,10 @@ app.get('/u/:user', (req, res, next) => {
 
 //app.listen(port, () => console.log(`HASHKINGS API listening on port ${port}!`))
 var state;
-var startingBlock = ENV.STARTINGBLOCK || 51950582; //GENESIS BLOCK
+var startingBlock = ENV.STARTINGBLOCK || 51950728; //GENESIS BLOCK
 const username = ENV.ACCOUNT || 'hashkings'; //account with all the SP
 const key = dhive.PrivateKey.from(ENV.skey); //active key for account
-const ago = ENV.ago || 51950582;
+const ago = ENV.ago || 51950728;
 const prefix = ENV.PREFIX || 'qwoyn_'; // part of custom json visible on the blockchain during watering etc..
 var client = new dhive.Client([
     "https://hive.roelandp.nl"
@@ -305,10 +305,11 @@ function userList() {
                 let userReport = report[username]
                 for (const property in report) {
                     //data = userReport[property].seeds
-                    console.log(property)
-                    //state.users.seeds = data
+                    if(property == username) {
+                        data = property.seeds
+                        console.log(data)
+                    }
                 }
-
             }
         }
 
