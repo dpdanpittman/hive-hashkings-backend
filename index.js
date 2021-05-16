3b6329c618d1691b137773d1900774b0b18cecc3
chocolatoso <serfresh@hotmail.com>
2021-05-15 22:20:18 -0500

    Update index.js

--- index.js
+++ index.js
@@ -160,10 +160,10 @@ app.use(cors());
 
 //app.listen(port, () => console.log(`HASHKINGS API listening on port ${port}!`))
 var state;
-var startingBlock = ENV.STARTINGBLOCK || 53928678; //GENESIS BLOCK
+var startingBlock = ENV.STARTINGBLOCK || 53928734; //GENESIS BLOCK
 const username = ENV.ACCOUNT || "hashkings"; //account with all the SP
 const key = dhive.PrivateKey.from(ENV.skey); //active key for account
-const ago = ENV.ago || 53928678;
+const ago = ENV.ago || 53928734;
 const prefix = ENV.PREFIX || "qwoyn_"; // part of custom json visible on the blockchain during watering etc..
 var client = new dhive.Client(
   [
