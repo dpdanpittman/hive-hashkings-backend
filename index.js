2121af01b64a25408d184e1444cabee4470b38fa
chocolatoso <serfresh@hotmail.com>
2021-06-11 21:57:51 -0500

    Update index.js

--- index.js
+++ index.js
@@ -848,9 +848,7 @@ app.get("/utest/:user", async (req, res, next) => {
     state.users[user].avatars = avatars;
     state.users[user].joints = joints;
     if (state.users[user].activeAvatar) {
-      let av = state.users[user].avatars.find(function (element) {
-        return element.id == state.users[user].activeAvatar.id;
-      });
+      let av =  state.users[user].activeAvatar;
 
       if (av) {
         state.users[user].activeAvatar = av;
